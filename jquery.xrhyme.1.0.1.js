7c6a9c31f90b10322f6f421f9b690545b90011d8
lancee <ly.lancee@gmail.com>
2012-03-17 21:28:21 +0800

    add callback

--- jquery.xrhyme.1.0.1.js
+++ jquery.xrhyme.1.0.1.js
@@ -15,28 +15,28 @@
     "use strict"
 
     $.xrhyme = function(xcontent, options) {
-    
-        // Reference this object 
+
+        // Reference this object
         var me = this;
-        
+
         // Private method
         var fn = {};
-        
+
         me.$x_cont = $(xcontent).addClass('x_container');
-        
+
         // Add a reverse reference to the DOM object
         me.$x_cont.data('X-Rhyme', me);
         me.$x_wrap = me.$x_cont.parent().addClass('x_wrapper');
-        
+
         // Initialization
         fn.init = function() {
             me.options = $.extend({}, $.xrhyme.defaults, options);
-            
+
             // Sum of each horizontal item's width
             me.width = 0;
-            
+
             // Preserve the offset of each horizontal item
-            me.offset = {};            
+            me.offset = {};
             me.$x_cont.children().each(function(i) {
                 $(this).addClass('x_item');
                 if(me.options.anchorMode) {
@@ -47,25 +47,25 @@
                 }
                 me.width += $(this).outerWidth(true);
             });
-            
-            // Reset width of horizontal container 
+
+            // Reset width of horizontal container
             me.$x_cont.width(me.width);
             fn.resetContent();
-            
+
             // Left and right ends flag
             me.endsFlag = 0;
-            
+
             if(me.options.navigationSelector) {
                 $(me.options.navigationSelector).each(function(i) {
                     var k = me.options.anchorMode ? $(this).attr('href').replace('#','') : i;
                     $(this).on('click', function(e) {
-                    
+
                         // Smooth scrolling to an anchor
                         me.$x_wrap.stop().animate({ scrollLeft: me.offset[k] },
                         {
-                            queue: false, 
-                            duration: me.options.animationTime, 
-                            easing: me.options.easing, 
+                            queue: false,
+                            duration: me.options.animationTime,
+                            easing: me.options.easing,
                             complete: function() {
                                 me.endsFlag = fn.endsFlag();
                                 if($.isFunction(me.options.animeComplete))
@@ -76,7 +76,7 @@
                     });
                 });
             }
-            
+
             window.onresize = fn.resetContent;
             document.onkeydown = fn.keyScroll;
             if($.browser.mozilla) {
@@ -85,15 +85,15 @@
                 document.onmousewheel = fn.mouseWheel;
             }
         };
-        
+
         // Return a number as endsFlag, it may be 0(leftEnd) 1(rightEnd) or value > 1
         fn.endsFlag = function() {
             return me.$x_wrap.scrollLeft()?
             (me.$x_cont.outerWidth(true) /( me.$x_wrap.scrollLeft() + $(window).width())):
             me.$x_wrap.scrollLeft();
         }
-        
-        // Fire the custom function 
+
+        // Fire the custom function
         // if scroll-left-end touch the border of each horizontal item
         // then update both ends (of horizontal container) flag
         fn.frontier = function() {
@@ -105,7 +105,7 @@
                         var lt = me.$x_wrap.scrollLeft() - speed/2;
                         var rt = me.$x_wrap.scrollLeft() + speed/2;
                         for (i in me.offset) {
-                        
+
                             // It will be negative within
                             if((me.offset[i] - lt)*(me.offset[i] - rt) < 0) {
                                 me.options.animeComplete.call(me, i, speed);
@@ -117,17 +117,17 @@
                 }
             }
         }();
-        
+
         // Reset width of horizontal container on window resize
         fn.resetContent = function() {
-        
+
             // Update step size for keyboard scrolling control
             me.perWidth = me.$x_wrap.width() / 10;
             var plus_width = $(window).width() - $('.x_item:last').outerWidth(true);
             if(plus_width>0)
             me.$x_cont.width(me.width + plus_width);
         };
-        
+
         // Scrolling page with mousewheel
         fn.mouseWheel = function(e) {
             e = e || window.event;
@@ -138,7 +138,7 @@
             fn.frontier(speed);
             return false;
         };
-        
+
         // Scrolling page with keyboard
         fn.keyScroll = function(e) {
             e = e || window.event;
@@ -154,51 +154,56 @@
                 default:
                 return;
             }
-            
+
         };
-        
+
         fn.init();
-        
+
     };
-    
+
     // X-Rhyme default settings
     $.xrhyme.defaults = {
-        
+
         // String of css selector for custom navigation, for instance: '#Nav li a'
         navigationSelector : '',
-        
+
         // The navigationSelector should be end with 'a'
         // links to anchor within horizontal items flow, so can be scrolling
         anchorMode : false,
-        
+
         // Percentage of mouseWheel speed(120), it's better between 0 & 1
         wheelSpeed : 0.5,
-        
+
         // Using easing function for scrolling transition, requires the easing plugin
         easing : 'swing',
-        
+
         // Milliseconds of scrolling transition
         animationTime : 600,
-        
+
         // Custom function, fire when the scrolling transition Completed
         animeComplete : ''
     };
-    
+
     $.fn.xrhyme = function(options, callback) {
         var xrhyme = $(this).data('X-Rhyme');
-        
+
         // Prevent multiple initializations
         return this.each(function(i) {
             if((typeof(options)).match('object|undefined')) {
                 if(!xrhyme) {
-                    (new $.xrhyme(this, options));
-                }else{}
-                
+                    xrhyme = new $.xrhyme(this, options);
+                    if (callback)
+                        callback.call(xrhyme);
+                }else{
+                    if (callback)
+                        callback.call(xrhyme);
+                }
+
             }else{
                 // If options is not a object...
             }
         });
-        
+
     };
-	
+
 })(jQuery);
